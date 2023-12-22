c4a36c4e6196288663be0cd0cb5326f1db0f6478
cmatthews20 <cmatthews20@mun.ca>
2023-12-22 13:58:14 -0330

    testing if pipeline runs wo randomization

---
+++ src/test_fixture.py
@@ -0,0 +1,6 @@
+# import pytest
+# import random
+
+# @pytest.fixture
+# def rand_val():
+#     return random.randint(1, 10)
--- src/test_parametrize.py
+++ src/test_parametrize.py
@@ -1,9 +1,9 @@
 import pytest
 import random
-
 from functions import square
 
-@pytest.mark.parametrize("num", [random.randint(1, 10)])
-def test_square_param(num):
-    result = square(num)
-    assert result == num ** 2
+# this causing problems in parallel pipeline
+# @pytest.mark.parametrize("num", [random.randint(1, 10)])
+# def test_square_param(num):
+#     result = square(num)
+#     assert result == num ** 2

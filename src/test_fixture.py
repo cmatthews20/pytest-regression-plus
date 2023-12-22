320d415340fee024373de416420dc21c31f0c974
cmatthews20 <cmatthews20@mun.ca>
2023-12-22 13:59:55 -0330

    test fixture randomization with xdist

--- src/test_fixture.py
+++ src/test_fixture.py
@@ -1,6 +1,11 @@
-# import pytest
-# import random
+import pytest
+import random
+from functions import square
 
-# @pytest.fixture
-# def rand_val():
-#     return random.randint(1, 10)
+@pytest.fixture
+def rand_val():
+    return random.randint(1, 10)
+
+def test_square_param(rand_val):
+    result = square(rand_val)
+    assert result == rand_val ** 2
